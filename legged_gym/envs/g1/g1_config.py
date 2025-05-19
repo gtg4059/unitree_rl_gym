b3a579a1b7faa351921b9d990f664914bb2de7d7
robotics <qkrtjdrhks21@gmail.com>
2025-05-19 11:14:10 +0900

    250519_1

--- legged_gym/envs/g1/g1_config.py
+++ legged_gym/envs/g1/g1_config.py
@@ -76,7 +76,7 @@ class G1RoughCfg( LeggedRobotCfg ):
         friction_range = [0.1, 1.25]
         randomize_base_mass = True
         added_mass_range = [-1., 3.]
-        push_robots = True
+        push_robots = False
         push_interval_s = 5
         max_push_vel_xy = 1.5
       
