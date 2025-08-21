30516ea902d49bef87c3292b92ed10ec6ca3d909
gtg4059 <qkrtjdrhks21@gmail.com>
2025-08-21 21:43:41 +0900

    250821_1

--- deploy/deploy_real/config_v1.py
+++ deploy/deploy_real/config_v1.py
@@ -22,7 +22,7 @@ class Config:
 
             self.policy_run = config["policy_run"].replace("{LEGGED_GYM_ROOT_DIR}", LEGGED_GYM_ROOT_DIR)
             self.policy_stop = config["policy_stop"].replace("{LEGGED_GYM_ROOT_DIR}", LEGGED_GYM_ROOT_DIR)
-            # self.policy_pickup = config["policy_pickup"].replace("{LEGGED_GYM_ROOT_DIR}", LEGGED_GYM_ROOT_DIR)
+            self.policy_pickup = config["policy_pickup"].replace("{LEGGED_GYM_ROOT_DIR}", LEGGED_GYM_ROOT_DIR)
 
             self.leg_joint2motor_idx = config["leg_joint2motor_idx"]
             self.kps = config["kps"]
