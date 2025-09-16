51dfad3dd43278614f614f59f8d2ee7db6360579
gtg4059 <qkrtjdrhks21@gmail.com>
2025-09-16 11:31:19 +0900

    13dof

--- deploy/deploy_real/deploy_real_13dof.py
+++ deploy/deploy_real/deploy_real_13dof.py
@@ -35,7 +35,7 @@ class Controller:
         self.policy_run = torch.jit.load(config.policy_run)
         self.policy_stop = torch.jit.load(config.policy_stop)
         # Initializing process variables
-        config.num_actions = 15
+        config.num_actions = 13
         self.qj = np.zeros(config.num_actions, dtype=np.float32)
         self.dqj = np.zeros(config.num_actions, dtype=np.float32)
         self.action = np.zeros(config.num_actions, dtype=np.float32)
@@ -204,7 +204,7 @@ class Controller:
         for i in range(len(self.config.leg_joint2motor_idx)):
             self.qj[i] = self.low_state.motor_state[self.config.leg_joint2motor_idx[i]].q
             self.dqj[i] = self.low_state.motor_state[self.config.leg_joint2motor_idx[i]].dq
-        for i in range(3):
+        for i in range(1):
             self.qj[i+len(self.config.leg_joint2motor_idx)] = self.low_state.motor_state[self.config.arm_waist_joint2motor_idx[i]].q
             self.dqj[i+len(self.config.leg_joint2motor_idx)] = self.low_state.motor_state[self.config.arm_waist_joint2motor_idx[i]].dq
 
@@ -231,19 +231,19 @@ class Controller:
         self.cmd[1] = self.remote_controller.lx * -1
         self.cmd[2] = self.remote_controller.rx * -1
 
-        num_actions = 15
+        num_actions = 13
         self.obs[:3] = ang_vel
         self.obs[3:6] = gravity_orientation
         self.obs[6 : 6 + num_actions] = qj_obs
         self.obs[6 + num_actions : 6 + num_actions * 2] = dqj_obs
         self.obs[6 + num_actions * 2 : 6 + num_actions * 3] = self.action
         self.obs[6 + num_actions * 3:9 + num_actions * 3] = self.cmd * 0
-        obs_tensor = torch.from_numpy(self.obs[:54]).unsqueeze(0)
+        obs_tensor = torch.from_numpy(self.obs[:48]).unsqueeze(0)
         # print("obs_tensor:",obs_tensor)
         self.action = self.policy_stop(obs_tensor).detach().numpy().squeeze()
         
         # transform action to target_dof_pos
-        target_dof_pos = np.concatenate([self.config.default_angles, self.config.arm_default_angles[:3]], axis=0) + self.action * self.config.action_scale #29
+        target_dof_pos = np.concatenate([self.config.default_angles, self.config.arm_default_angles[:1]], axis=0) + self.action * self.config.action_scale #13
         # target_dof_pos = self.action * self.config.action_scale #29
         # print("target_dof_pos:",*target_dof_pos)
 
@@ -259,7 +259,7 @@ class Controller:
             self.low_cmd.motor_cmd[motor_idx].kd = self.config.kds[i]
             self.low_cmd.motor_cmd[motor_idx].tau = 0
         # print("arm_waist_joint2motor_idx")
-        for i in range(3):
+        for i in range(1):
             # print("waist:",np.clip(target_dof_pos[i+len(self.config.leg_joint2motor_idx)],self.config.arm_waist_limits_low[i],self.config.arm_waist_limits_high[i]))
             motor_idx = self.config.arm_waist_joint2motor_idx[i]
             self.low_cmd.motor_cmd[motor_idx].q = np.clip(target_dof_pos[i+len(self.config.leg_joint2motor_idx)],
@@ -268,13 +268,13 @@ class Controller:
             self.low_cmd.motor_cmd[motor_idx].kp = self.config.arm_waist_kps[i]
             self.low_cmd.motor_cmd[motor_idx].kd = self.config.arm_waist_kds[i]
             self.low_cmd.motor_cmd[motor_idx].tau = 0
-        for i in range(len(self.config.arm_waist_joint2motor_idx)-3): # 12
+        for i in range(len(self.config.arm_waist_joint2motor_idx)-1): # 14
             # print("arm:",self.config.arm_default_angles[i+3])
-            motor_idx = self.config.arm_waist_joint2motor_idx[i+3]
-            self.low_cmd.motor_cmd[motor_idx].q = self.config.arm_default_angles[i+3]
+            motor_idx = self.config.arm_waist_joint2motor_idx[i+1]
+            self.low_cmd.motor_cmd[motor_idx].q = self.config.arm_default_angles[i+1]
             self.low_cmd.motor_cmd[motor_idx].qd = 0
-            self.low_cmd.motor_cmd[motor_idx].kp = self.config.arm_waist_kps[i+3]
-            self.low_cmd.motor_cmd[motor_idx].kd = self.config.arm_waist_kds[i+3]
+            self.low_cmd.motor_cmd[motor_idx].kp = self.config.arm_waist_kps[i+1]
+            self.low_cmd.motor_cmd[motor_idx].kd = self.config.arm_waist_kds[i+1]
             self.low_cmd.motor_cmd[motor_idx].tau = 0
 
 
